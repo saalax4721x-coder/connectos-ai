acd2c6d35d1d93b86c6894b55c339d3ebb939c63
saalax4721x-coder <saalax4721x@gmail.com>
2026-08-18 01:15:38 +0300

    feat(v3): add business mode plan

---
+++ src/v3/modes/business.ts
@@ -0,0 +1,1 @@
+export interface BusinessPlan{userId:string;customerTargets:string[];partnerTargets:string[];supplierTargets:string[];competitors:string[];marketSignals:string[];nextActions:string[];}

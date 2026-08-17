739946b60bae245a3cd3c1a0db5f64569b70aa74
saalax4721x-coder <saalax4721x@gmail.com>
2026-08-18 01:15:45 +0300

    feat(v3): add founder mode plan

---
+++ src/v3/modes/founder.ts
@@ -0,0 +1,1 @@
+export interface FounderPlan{userId:string;cofounderTargets:string[];talentTargets:string[];customerTargets:string[];investorTargets:string[];partnerTargets:string[];nextActions:string[];}

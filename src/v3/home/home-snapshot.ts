b6d289741b0dcfcb66afa3c01d55ba74d9acfef5
saalax4721x-coder <saalax4721x@gmail.com>
2026-08-18 01:10:31 +0300

    feat(v3): add personalized home snapshot

---
+++ src/v3/home/home-snapshot.ts
@@ -0,0 +1,1 @@
+export interface HomeSnapshot{topOpportunityId?:string;topPersonId?:string;topFollowupId?:string;topSignalId?:string;activeDealIds:string[];collaborationIds:string[];generatedAt:string;}
